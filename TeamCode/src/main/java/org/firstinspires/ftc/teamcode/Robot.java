24bce35133fc8ef7a90d3090ec63018d1256adc5
Bryan Qi <bryanqi@johns-MacBook-Pro.local>
2023-08-04 22:20:20 -0400

    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,26 +1,53 @@
 package org.firstinspires.ftc.teamcode;
 
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Robot{
-    public DcMotorEx leftFront;
-    public DcMotorEx leftBack;
-    public DcMotorEx rightFront;
-    public DcMotorEx rightBack;
 
-    Servo claw;
+    public DcMotorEx leftBack, leftFront, rightBack, rightFront;
+    public BNO055IMU imu;
+    Orientation currentAngle;
+
+    LinearOpMode linearOpMode;
+    HardwareMap hardwareMap;
+    Telemetry telemetry;
 
 
-    public Robot (HrdwareMap hardwareMap, LinearOpMode linearOpMode) {
+    public Robot(HardwareMap hardwareMap, LinearOpMode linearOpMode) {
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightRear");
         leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
-        claw = hardwareMap.get(Servo.class, "claw");
 
-        leftFront.setDirection(DcMotor.Direction.REVERSE); //reverses da motor
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+        currentAngle = imu.getAngularOrientation();
+        this.linearOpMode = linearOpMode;
+        this.hardwareMap = hardwareMap;
+
     }
 }
 
