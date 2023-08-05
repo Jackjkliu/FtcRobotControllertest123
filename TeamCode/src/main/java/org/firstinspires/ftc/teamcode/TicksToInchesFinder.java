d0cdf2efd608a6adf2a11e458fe76db722fc827e
Bryan Qi <bryanqi@johns-MacBook-Pro.local>
2023-08-05 19:40:53 -0400

    tickstoinches

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TicksToInchesFinder.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp (name = "TicksToInchesFinder")
+public class TicksToInchesFinder extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+    }
+}
